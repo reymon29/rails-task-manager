b4c5d3b657f2c6c798599941ed19aa651bd65a42
Reyes Montemayor <sjpete1234@hotmail.com>
2018-05-22 16:11:54 +0900

    logic completed with refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get "tasks", to: "tasks#index",  as: :tasks
 
-  get "tasks", to: "tasks#index",  as: :tasks
+  # get "tasks/new", to: "tasks#new",  as: :new_task
+  # post "tasks", to: "tasks#create"
 
-  get "tasks/new", to: "tasks#new",  as: :new_task
-  post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: :task
 
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
 
-  delete "tasks/:id", to: "tasks#destroy"
-
-  root to: "tasks#index"
+  # root to: "tasks#index"
 end
